d7436db750affd99f2e8b477d4458f74709b07ac
AARYA RAJOJU <aaryarajoju@gmail.com>
2021-07-02 18:36:50 +0530

    added crud operations

---
+++ src/queries.js
@@ -0,0 +1,76 @@
+const {user, host, database, password, port} = require("./config.json");
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: user,
+    host: host,
+    database: database,
+    password: password,
+    port: port,
+});
+
+const getTasks = (request, response) => {
+    pool.query('SELECT * FROM tasks ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows)
+    });
+};
+
+const getTaskById = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('SELECT * FROM tasks WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+};
+
+const createTask = (request, response) => {
+    const { taskname, status } = request.body;
+
+    pool.query('INSERT INTO tasks (taskname, status) VALUES ($1, $2)', [taskname, status], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(201).send(`Task added with ID: ${results.insertId}`);
+    });
+};
+
+const updateTask = (request, response) => {
+    const id = parseInt(request.params.id);
+    const { taskname, status } = request.body;
+
+    pool.query(
+        'UPDATE tasks SET taskname = $1, status = $2 WHERE id = $3',
+        [taskname, status, id],
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+            response.status(200).send(`Task modified with ID: ${id}`);
+        }
+    );
+};
+
+const deleteTask = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELETE FROM tasks WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).send(`Task deleted with ID: ${id}`);
+    });
+};
+
+module.exports = {
+    getTasks,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask,
+};
